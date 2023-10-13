0dcb040b33455eda1e65accbae36ee51c26e7fe8
aryu-shan <145306193+aryu-shan@users.noreply.github.com>
2023-10-13 21:17:22 +0900

    ユーザー管理機能（3回目修正）

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
    validates :password,format: { with: /\A(?=.*[a-z])(?=.*\d)(?![\p{Katakana}])\w+\z/i, 
               message: "Password is invalid. Include both letters and numbers"
                }, on: :create
-   validates :family_name,        presence: true, format:{ with: /\A[ぁ-んァ-ヶ一-龥]+\z/, message: "Family name is invalid. Input full-width characters" }
-   validates :first_name,         presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/, message: "First name is invalid. Input full-width characters" }
+   validates :family_name,        presence: true, format:{ with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z|ヶ/, message: "Family name is invalid. Input full-width characters" }  
+   validates :first_name,         presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z|ヶ/, message: "First name is invalid. Input full-width characters" }
    validates :family_name_kana,   presence: true, format:{ with: /\A[\p{katakana}\p{blank}]+\z/, message: 'Please enter in the full-width katakana' }
    validates :first_name_kana,    presence: true, format: { with: /\A[\p{katakana}\p{blank}]+\z/, message: 'Please enter in the full-width katakana' }
    validates :birth_day,          presence: true

c056d71d5f04963c8a40bb28db7dffe40b389858
aryu-shan <145306193+aryu-shan@users.noreply.github.com>
2023-10-19 15:46:18 +0900

    商品情報編集機能(1回目修正)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
+  before_action :set_item, only: [:edit, :update,:show]
 
   def index
     @items= Item.all.order(created_at: :desc)
@@ -19,11 +20,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item =Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     if user_signed_in? && current_user == @item.user
       render :edit
     else
@@ -32,7 +31,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
     redirect_to item_path(@item)
   else
@@ -41,11 +39,14 @@ class ItemsController < ApplicationController
 end
 
 
-
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :explanation, :category_id, :condition_id, :price, :fee_burden_id, :area_id, :handling_time_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
   def move_to_index

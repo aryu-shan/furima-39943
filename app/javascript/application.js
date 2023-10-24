25b20c5f7d42a1452e801546f8b555202bd04ca7
aryu-shan <145306193+aryu-shan@users.noreply.github.com>
2023-10-24 18:08:22 +0900

    fix: file name

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "./tax.js"
 import "card"
+import "tax"
